0dd4021aadd2a1f15bd8b9023268806fb55a36e4
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-10 01:40:04 -0400

    Update MyController.java

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -59,12 +59,12 @@ public class MyController {
                 output.add("Read from DB: " + rs.getTimestamp("tick"));
             }
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS cases (caseId SERIAL PRIMARY KEY NOT NULL UNIQUE," + 
-                                                                "firstname TEXT NOT NULL"+
-                                                                "lastname TEXT NOT NULL"+
+                                                                "firstname TEXT NOT NULL,"+
+                                                                "lastname TEXT NOT NULL,"+
                                                                 "description TEXT NOT NULL)");
             /*stmt.executeUpdate("INSERT INTO cases(caseId,firstname,lastname,description) VALUES (1,'a','b','c')");
             rs = stmt.executeQuery("SELECT *  FROM case where caseId = 1");
-            output.add("Read from DB: " + rs.getTimestamp("caseId") + " " + rs.getString("firstname"));/*
+            output.add("Read from DB: " + rs.getTimestamp("caseId") + " " + rs.getString("firstname"));*/
             model.put("records", output);
             return "searchResults";
         } catch (Exception e) {
