84f1f82bf593bd35e8104118902046b87151ca6a
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-10 02:26:52 -0400

    Update MyController.java

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -58,11 +58,11 @@ public class MyController {
             while (rs.next()) {
                 output.add("Read from DB: " + rs.getTimestamp("tick"));
             }
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS cases (caseId INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT," + 
+            /*stmt.executeUpdate("CREATE TABLE IF NOT EXISTS cases (caseId INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT," + 
                                                                  "firstname VARCHAR(15) NOT NULL,"+
                                                                  "lastname VARCHAR(15) NOT NULL," +
                                                                  "description VARCHAR(255) NOT NULL)");
-            /*stmt.executeUpdate("INSERT INTO cases (firstname,lastname,description) VALUES ('a','b','c')");
+            stmt.executeUpdate("INSERT INTO cases (firstname,lastname,description) VALUES ('a','b','c')");
             rs = stmt.executeQuery("SELECT *  FROM case where caseId = 1");
             output.add("Read from DB: " + rs.getTimestamp("caseId") + " " + rs.getString("firstname"));*/
             model.put("records", output);
