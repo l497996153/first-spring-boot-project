9fa14444a6059360b12ac37cb0e97c4f1beb97ba
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-10 03:14:24 -0400

    Update MyController.java

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -65,7 +65,7 @@ public class MyController {
                                                                  "PRIMARY KEY(caseId));");
             stmt.executeUpdate("INSERT INTO cases VALUES (1,'a','b','c');");
             int id = 1;
-            ResultSet rs1 = stmt.executeQuery("SELECT caseId,firstname FROM cases;");
+            //ResultSet rs1 = stmt.executeQuery("SELECT caseId,firstname FROM cases;");
             //rs = stmt.executeQuery("SELECT * FROM cases WHERE caseId = "+id+";");
             /*output.add("Read from DB: " + rs1.getInt("caseId") + " " + rs1.getString("firstname"));*/
             model.put("records", output);
